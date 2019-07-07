37313aaeca5264ff65af6af6ab9da4824dc08b32
Ritesh Mukim <ritesh.mukim@nissanmotor.com>
2019-07-07 21:17:50 +0530

    Updates

---
+++ precache-manifest.5d4060c876b0b010301ef3b0480f02b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828fa53c3a313c3527b27d1b49816f96",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "a69356fde44515f0223d",
+    "url": "/website/static/css/main.73db2d6b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/website/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a69356fde44515f0223d",
+    "url": "/website/static/js/main.4226ce71.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  }
+]);
